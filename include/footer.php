20ac23acfb15f47922fd3173e799124ba0974e57
hrik-das <emptynull01@gmail.com>
2025-05-10 18:07:18 +0530

    changes and some minor issues fixed

--- include/footer.php
+++ include/footer.php
@@ -1,8 +1,8 @@
 <div class="container-fluid mt-5 bg-white">
     <div class="row">
         <div class="col-lg-4 p-4">
-            <h3 class="new-font fw-bold fs-3 mb-3">Godlike Restaurant</h3>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure, quia. Illum nesciunt tempora provident cupiditate porro repellendus asperiores hic odio tempore recusandae explicabo, vel nobis odit reprehenderit quibusdam, debitis id!</p>
+            <h3 class="new-font fw-bold fs-3 mb-3"><?php echo $settings_result["site_title"]; ?></h3>
+            <p><?php echo $settings_result["site_about"]; ?></p>
         </div>
         <div class="col-lg-4 p-4">
             <h5 class="mb-3">Support</h5>
@@ -25,9 +25,7 @@
                 if ($contact_result["twitter"] != "") {
                     echo<<<data
                         <a href="$contact_result[twitter]" target="_blank" class="d-inline-block text-dark text-decoration-none">
-                            <span class="badge bg-light text-dark fs-6 p-2">
-                                <i class="bi bi-twitter"></i> Twitter
-                            </span>
+                            <i class="bi bi-twitter"></i> Twitter
                         </a>
                     data;
                 }
--- include/header.php
+++ include/header.php
@@ -1,16 +1,9 @@
-<?php
-    require_once("admin/include/connect.php");
-    require_once("admin/include/essential.php");
-
-    $contact_query = "SELECT * FROM `contact_details` WHERE `sr_no`=?";
-    $contact_values = [1];
-    $contact_result = mysqli_fetch_assoc(select($contact_query, $contact_values, "i"));
-?>
-
 <!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-light bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
     <div class="container-fluid">
-        <a class="navbar-brand new-font me-5 fw-bold fs-3" href="index.php">Godlike Restaurant</a>
+        <a class="navbar-brand new-font me-5 fw-bold fs-3" href="index.php">
+            <?php echo $settings_result["site_title"]; ?>
+        </a>
         <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -44,7 +37,7 @@
 <div class="modal fade" id="registerModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
-            <form action="">
+            <form action="" id="register-form">
                 <div class="modal-header">
                     <h5 class="modal-title d-flex align-items-center" id="staticBackdropLabel">
                         <i class="bi bi-person-vcard-fill fs-3 me-2"></i> User Registration
@@ -61,55 +54,55 @@
                                 <label class="form-label">
                                     <i class="bi bi-person-fill"></i> Username
                                 </label>
-                                <input type="text" class="form-control shadow-none">
+                                <input type="text" name="username" class="form-control shadow-none" required>
                             </div>
                             <div class="col-md-6 p-0 mb-3">
                                 <label class="form-label">
                                     <i class="bi bi-envelope-at-fill"></i> Email address
                                 </label>
-                                <input type="email" class="form-control shadow-none">        
+                                <input type="email" name="email" class="form-control shadow-none" required>        
                             </div>
                             <div class="col-md-6 ps-0 mb-3">
                                 <label class="form-label">
                                     <i class="bi bi-telephone-inbound-fill"></i> Contact Details
                                 </label>
-                                <input type="number" class="form-control shadow-none">
+                                <input type="number" name="phone" class="form-control shadow-none" required>
                             </div>
                             <div class="col-md-6 p-0 mb-3">
                                 <label class="form-label">
                                     <i class="bi bi-person-bounding-box"></i> Profile Picture
                                 </label>
-                                <input type="file" class="form-control shadow-none">        
+                                <input type="file" name="profile" class="form-control shadow-none" accept=".jpg, .png, .jpeg, .webp" required>
                             </div>
                             <div class="col-md-12 p-0 mb-3">
                                 <label class="form-label">
                                     <i class="bi bi-house-add-fill"></i> Permanent Address
                                 </label>
-                                <textarea class="form-control shadow-none" rows="1"></textarea>        
+                                <textarea name="address" class="form-control shadow-none" rows="1" required></textarea>        
                             </div>
                             <div class="col-md-6 ps-0 mb-3">
                                 <label class="form-label">
                                     <i class="bi bi-geo-alt-fill"></i> Pincode
                                 </label>
-                                <input type="number" class="form-control shadow-none">
+                                <input type="number" name="pincode" class="form-control shadow-none" required>
                             </div>
                             <div class="col-md-6 p-0 mb-3">
                                 <label class="form-label">
                                     <i class="bi bi-calendar-date"></i> Date of Birth
                                 </label>
-                                <input type="date" class="form-control shadow-none">        
+                                <input type="date" name="dob" class="form-control shadow-none" required>        
                             </div>
                             <div class="col-md-6 ps-0 mb-3">
                                 <label class="form-label">
                                     <i class="bi bi-shield-lock-fill"></i> Password
                                 </label>
-                                <input type="password" class="form-control shadow-none">
+                                <input type="password" name="password" class="form-control shadow-none" required>
                             </div>
                             <div class="col-md-6 p-0 mb-3">
                                 <label class="form-label">
                                     <i class="bi bi-check-square-fill"></i> Confirm Password
                                 </label>
-                                <input type="password" class="form-control shadow-none">        
+                                <input type="password" name="confirm_password" class="form-control shadow-none" required>        
                             </div>
                         </div>
                         <div class="text-center my-1">
