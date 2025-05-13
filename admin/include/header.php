7639a1af6809b292091b011bd162d34bfaf0dae3
hrik-das <emptynull01@gmail.com>
2025-05-13 16:04:15 +0530

    new bookings and refund and booking record section added

--- admin/include/header.php
+++ admin/include/header.php
@@ -16,6 +16,27 @@
                         <a class="nav-link text-white" href="dashboard.php">Dashboard</a>
                     </li>
                     <li class="nav-item">
+                        <button class="btn text-white px-3 w-100 shadow-none text-start d-flex align-items-center justify-content-between" type="button" data-bs-toggle="collapse" data-bs-target="#bookingLink">
+                            <span>Bookings</span>
+                            <span>
+                                <i class="bi bi-caret-down-fill"></i>
+                            </span>
+                        </button>
+                        <div class="collapse show px-3 small mb-1" id="bookingLink">
+                            <ul class="nav nav-pills flex-column rounded border border-secondary">
+                                <li class="nav-item">
+                                    <a href="new_booking.php" class="nav-link text-light">New Bookings</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="refund_booking.php" class="nav-link text-light">Refund Bookings</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="booking_record.php" class="nav-link text-light">Booking Records</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="nav-item">
                         <a class="nav-link text-white" href="features_facilities.php">Features & Facilities</a>
                     </li>
                     <li class="nav-item">
